10df2ab1654e14e35436907ea615c5792027c317
RamONcoche <joaozinhocavalcante.jovc@gmail.com>
2019-10-13 01:39:18 -0300

    Lora Server

--- projeto/projeto.ino
+++ projeto/projeto.ino
@@ -1,11 +1,60 @@
+//Arduino Raspberry Pi wireless Comunnication through LoRa - SX1278
+//Send 0 to 9 from Arduino through Radio head LoRa without ACK
+//Code for: www.circuitdigest.com
+//Dated: 19-4-20198
 
+#include <SPI.h> //Import SPI librarey 
+#include <RH_RF95.h> // RF95 from RadioHead Librarey 
 
-void setup() {
-  // put your setup code here, to run once:
+#define RFM95_CS 10 //CS if Lora connected to pin 10
+#define RFM95_RST 9 //RST of Lora connected to pin 9
+#define RFM95_INT 2 //INT of Lora connected to pin 2
 
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup() 
+{
+ 
+//Initialize Serial Monitor
+  Serial.begin(9600);
+  
+// Reset LoRa Module 
+  pinMode(RFM95_RST, OUTPUT); 
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+//Initialize LoRa Module
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  
+
+ //Set the default frequency 434.0MHz
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+
+  rf95.setTxPower(18); //Transmission power of the Lora Module
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+char value = 48;
 
+void loop()
+{
+  Serial.print("Send: ");
+  char radiopacket[1] = char(value)};
+  rf95.send((uint8_t *)radiopacket, 1);
+
+    
+  delay(1000);
+  value++;
+  if (value > '9')
+  value = 48;
 }
